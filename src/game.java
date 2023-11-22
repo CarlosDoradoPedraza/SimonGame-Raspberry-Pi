5a6c38b23aa0138ff3a636b44c45c81f51827fd4
CarlosDoradoPedraza <carlosdoradopedraza@gmail.com>
2023-11-22 09:32:57 +0000

    Revert "New calculation"
    This reverts commit fa82adc2f47245447d77d30f982c2f986817fe08.

--- src/game.java
+++ src/game.java
@@ -4,11 +4,6 @@ public class game {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		int a, b, c;
-		a = 5;
-		b = 2;
-		c = a * b;
-		System.out.println(c);
 	}
 
 }
